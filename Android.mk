b321595c0f37018b4956fcf95f01c073bb328cf8
Stephen Gowen <dev.sgowen@gmail.com>
2018-06-04 16:56:23 -0400

    Adding LOCAL_SHARED_LIBRARIES

--- Android.mk
+++ Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_LDLIBS := -llog
+LOCAL_SHARED_LIBRARIES := libcutils libutils libbinder libstlport
 LOCAL_MODULE := playxsvf
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := ports.c micro.c lenval.c 
 include $(BUILD_EXECUTABLE)
-
